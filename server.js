da382c4f8f7397e461f1dfecc8f20c52b9b20f7e
jruel006 <jruel006@ucr.edu>
2016-07-06 18:51:49 -0700

    tryna get the boiler plate to work

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+
+
+app.set('view engine', 'ejs')
+
+/*This is so we can tie our front end dependencies together */
+app.use(express.static('views'))
+app.use(express.static('bower_components'))
+
+app.get('/', function(req, res) {
+    res.send('test')
+});
+
+app.post('/', function(req, res) {
+    // do alchemy stuff here
+});
+
+
+app.listen(3000, function(){
+    console.log('we in nigga')
+})
